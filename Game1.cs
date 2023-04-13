01671521f557fd87dc93915564758930d486b01f
tsuchu0 <abilbro005@gmail.com>
2023-04-13 00:50:50 -0400

    finished projectile work

--- Game1.cs
+++ Game1.cs
@@ -22,7 +22,11 @@ namespace deeepio
         #endregion
         
         Texture2D cursorTexture, projTexture;
+        SpriteFont font;
         Rectangle cursorRect;
+        MouseState mouseState, prevMouseState;
+        KeyboardState keyState;
+        List<Projectile> projList = new List<Projectile>();
 
         public Game1()
         {
@@ -55,6 +59,8 @@ namespace deeepio
             pTexture = Content.Load<Texture2D>("player");
             cursorTexture = Content.Load<Texture2D>("cursor");
             projTexture = Content.Load<Texture2D>("projectile");
+
+            font = Content.Load<SpriteFont>("font");
         }
 
         protected override void Update(GameTime gameTime)
@@ -62,9 +68,12 @@ namespace deeepio
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            KeyboardState keyState = Keyboard.GetState();
-            MouseState mouseState = Mouse.GetState();
-            
+            prevMouseState = mouseState;
+
+            keyState = Keyboard.GetState();
+            mouseState = Mouse.GetState();
+
+            #region Player Movement & Rotation
             Vector2 mousePosition = new Vector2(mouseState.X, mouseState.Y);
             Vector2 distance = mousePosition - new Vector2(pRect.X, pRect.Y);
             pRotation = (float)Math.Atan2(distance.Y, distance.X) + (float)Math.PI/2;
@@ -81,9 +90,26 @@ namespace deeepio
             if (keyState.IsKeyDown(Keys.D)) {
                 pRect.X += 2;
             }
+            #endregion
 
+            #region Cursor Stuff
             cursorRect.X = (int)mousePosition.X - 12;
             cursorRect.Y = (int)mousePosition.Y - 12;
+            #endregion
+
+            #region Projectile Stuff
+            // Player Projectiles
+            if (mouseState.LeftButton == ButtonState.Released && prevMouseState.LeftButton == ButtonState.Pressed)
+            {
+                projList.Add(new Projectile(pRect, mouseState, gameTime));
+            }
+
+            // Update Projectiles
+            foreach (Projectile p in projList)
+            {
+                p.Move(gameTime);
+            }
+            #endregion
 
 
             base.Update(gameTime);
@@ -98,6 +124,11 @@ namespace deeepio
             _spriteBatch.Draw(pTexture, pRect, null, Color.White, pRotation, new Vector2(31, 51), SpriteEffects.None, 0.0f);
             _spriteBatch.Draw(cursorTexture, cursorRect, Color.White);
 
+            foreach (Projectile p in projList)
+            {
+                _spriteBatch.Draw(projTexture, p.Position, Color.White);
+            }
+
             _spriteBatch.End();
 
             base.Draw(gameTime);
@@ -110,18 +141,28 @@ namespace deeepio
         public Vector2 Direction { get; set; }
         public Vector2 Origin { get; set; }
         public float Speed { get; set; }
-        public int StartTime { get; set; }
+        public float StartTime { get; set; }
 
         public Projectile(Rectangle originRect, MouseState ms, GameTime gt)
         {
-            this.Direction = new Vector2(ms.X, ms.Y) - new Vector2(originRect.Width / 2, originRect.Top);
-            this.Direction.Normalize();
-            this.StartTime = gt.TotalGameTime.TotalMilliseconds;
+            /*
+             * nv has to exist, due to the fact that .Normalize() does not
+             * edit the Vector2 it is called on, so instead direction 
+             * must be copied, then normalized, and finally reset.
+             */
+            this.Direction = new Vector2(ms.X, ms.Y) - new Vector2(originRect.X, originRect.Y);
+            Vector2 nv = this.Direction;
+            nv.Normalize();
+            this.Direction = nv;
+
+            this.StartTime = (int)gt.TotalGameTime.TotalMilliseconds;
+            this.Speed = 0.58f;
+            this.Origin = new Vector2(originRect.X, originRect.Y);
         }
 
         public void Move(GameTime gt)
         {
-            this.Position = this.Origin + this.Speed * (gt.TotalGameTime.TotalMilliseconds - this.StartTime) * this.Direction;
+            this.Position = this.Origin + (this.Speed * ((float)gt.TotalGameTime.TotalMilliseconds - this.StartTime) * this.Direction);
         }
     }
 }
